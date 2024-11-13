c80e92a7ff08388bb7a56c0acb636c000d8de51e
Vicram <vicramanjohfue@gmail.com>
2024-11-14 00:09:11 +0200

    create connect to mongodb

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,18 @@
+import dotenv from "dotenv";
 import express from "express";
+import mongoose from "mongoose";
+
+dotenv.config();
+
+const uri = process.env.MONGODB_URL;
+mongoose
+  .connect(uri)
+  .then(() => {
+    console.log("Connected to MongoDB!");
+  })
+  .catch((err) => {
+    console.error("Connection failed:", err.message);
+  });
 
 const app = express();
 
